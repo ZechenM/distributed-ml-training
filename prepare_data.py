3766dd7910d7024b51d5cdefa8c6b9973224e844
Jinyan Yi <alexjy.yi@mail.utoronto.ca>
2025-02-27 18:32:09 -0500

    create test dataloader

--- prepare_data.py
+++ prepare_data.py
@@ -11,19 +11,28 @@ def prepare_mnist_data():
         root="./data", train=True, download=True, transform=transform
     )
 
+    mnist_test = datasets.MNIST(root="./data", train=False, download=True, transform=transform)
+
     # Split the dataset into 3 parts
     split_sizes = [20000, 20000, 20000]  # Each worker gets 20,000 samples
     split_datasets = random_split(mnist_dataset, split_sizes)
 
     # Create dataloaders for each worker
-    dataloaders = [DataLoader(ds, batch_size=64, shuffle=True) for ds in split_datasets]
+    train_dataloaders = [DataLoader(ds, batch_size=64, shuffle=True) for ds in split_datasets]
 
-    return dataloaders
+    test_dataloader = DataLoader(mnist_test, batch_size=64, shuffle=False)
+    return train_dataloaders, test_dataloader
 
 
 if __name__ == "__main__":
-    dataloaders = prepare_mnist_data()
-    for i, dataloaders in enumerate(dataloaders):
+    train_dataloaders, test_dataloader = prepare_mnist_data()
+    
+    for i, dataloaders in enumerate(train_dataloaders):
         with open(f'dataloader_{i}.pkl', 'wb') as f:
             pickle.dump(dataloaders, f)
-    print("Data prepared and split into 3 dataloaders.")
+    print("Train Data prepared and split into 3 dataloaders.")
+
+    with open('dataloader_test.pkl', 'wb') as f:
+        pickle.dump(test_dataloader, f)
+    print("Test Data prepared.")
+
